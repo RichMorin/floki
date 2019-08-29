458f42d0f01119acea55e1bbd601ff8edc6e0c25
Philip Sampaio Silva <philip.sampaio@gmail.com>
2019-08-28 22:07:48 -0300

    WIP: processing the tree builder @ initial mode
    This is the start of the tree builder.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,4 @@
+Application.put_env(:ex_unit, :module_load_timeout, 120_000)
 # fast_html uses a C-Node worker for parsing, so starting the application
 # is necessary for it to work
 if Application.get_env(:floki, :html_parser) == Floki.HTMLParser.FastHtml do
